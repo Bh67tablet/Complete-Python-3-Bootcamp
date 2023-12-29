dbcc60778f949502c760ff0418d6436f64d0c33a
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-29 23:52:24 +0100

    Update setup_jupyter_ubuntu_1.sh

--- setup_jupyter_ubuntu_1.sh
+++ setup_jupyter_ubuntu_1.sh
@@ -10,25 +10,5 @@ sudo apt-get install -y bzip2
 #
 wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 #
-# bash Anaconda3-2019.03-Linux-x86_64.sh
-chmod u+x *.exp
-./script.exp
+bash Anaconda3-2019.03-Linux-x86_64.sh
 #
-source ~/.bashrc
-jupyter notebook --generate-config
-cat > $HOME/.jupyter/jupyter_notebook_config.py << EOF
-conf = get_config()
-conf.NotebookApp.ip = '0.0.0.0'
-conf.NotebookApp.password = u'YOUR PASSWORD HASH'
-conf.NotebookApp.port = 8888
-EOF
-#
-# set your password
-#jupyter notebook password
-./setJupyterpw.exp
-#
-# start:
-jupyter notebook &
-#
-# https://github.com/Mohdwajtech/Complete-Python-3-Bootcamp.git
-# git clone https://github.com/Bh67tablet/Complete-Python-3-Bootcamp.git
