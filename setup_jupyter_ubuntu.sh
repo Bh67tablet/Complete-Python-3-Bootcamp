f7c112c9e1801283c8a172473597b02f64344662
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-29 23:34:56 +0100

    Update setup_jupyter_ubuntu.sh

--- setup_jupyter_ubuntu.sh
+++ setup_jupyter_ubuntu.sh
@@ -4,10 +4,10 @@
 #
 # https://dataschool.com/data-modeling-101/running-jupyter-notebook-on-an-ec2-server/
 #
-#sudo apt-get install bzip2
-#
 sudo apt-get update -y
 sudo apt-get install -y expect
+sudo apt-get install -y bzip2
+#
 wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 #
 # bash Anaconda3-2019.03-Linux-x86_64.sh
