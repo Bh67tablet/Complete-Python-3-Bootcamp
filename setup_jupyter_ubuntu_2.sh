8f58c7e07c54bea06b3ce87aa6d279c7dbd21705
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-30 01:00:10 +0100

    Update setup_jupyter_ubuntu_2.sh

--- setup_jupyter_ubuntu_2.sh
+++ setup_jupyter_ubuntu_2.sh
@@ -1,5 +1,4 @@
 #/bin/bash!
-source ~/.bashrc
 jupyter notebook --generate-config
 cat > $HOME/.jupyter/jupyter_notebook_config.py << EOF
 conf = get_config()
@@ -9,11 +8,11 @@ conf.NotebookApp.port = 8888
 EOF
 #
 # set your password
-#jupyter notebook password
-./setJupyterpw.exp
+# jupyter notebook password
+#./setJupyterpw.exp
 #
 # start:
-jupyter notebook &
+# nohup jupyter notebook --no-browser --port=8889 --ip=0.0.0.0 &
 #
 # https://github.com/Mohdwajtech/Complete-Python-3-Bootcamp.git
 # git clone https://github.com/Bh67tablet/Complete-Python-3-Bootcamp.git
