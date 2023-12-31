675caf2d24b5343e9d4c6a8ee994440f13f73621
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-31 12:58:20 +0100

    Update setup_jupyter_ubuntu.sh

--- setup_jupyter_ubuntu.sh
+++ setup_jupyter_ubuntu.sh
@@ -1,34 +1,13 @@
 #/bin/bash!
 #
-# only Debian Linux (apt) !!!
-#
-# https://dataschool.com/data-modeling-101/running-jupyter-notebook-on-an-ec2-server/
-#
-sudo apt-get update -y
-sudo apt-get install -y expect
-sudo apt-get install -y bzip2
-#
-wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
-#
-# bash Anaconda3-2019.03-Linux-x86_64.sh
+wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
+# bash Anaconda3-2023.09-0-Linux-x86_64.sh
 chmod u+x *.exp
 ./script.exp
-#
 source ~/.bashrc
+conda install jupyter -yq
+#
 jupyter notebook --generate-config
-cat > $HOME/.jupyter/jupyter_notebook_config.py << EOF
-conf = get_config()
-conf.NotebookApp.ip = '0.0.0.0'
-conf.NotebookApp.password = u'YOUR PASSWORD HASH'
-conf.NotebookApp.port = 8888
-EOF
 #
-# set your password
-#jupyter notebook password
-./setJupyterpw.exp
-#
-# start:
-jupyter notebook &
-#
-# https://github.com/Mohdwajtech/Complete-Python-3-Bootcamp.git
-# git clone https://github.com/Bh67tablet/Complete-Python-3-Bootcamp.git
+cd Complete-Python-3-Bootcamp/
+nohup jupyter notebook --no-browser --port=8888 --ip=0.0.0.0 &
