4aa0fd2cab172dae19ce26ae3380e399380188ee
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-31 15:03:07 +0100

    Update setup_jupyter_ubuntu.sh

--- setup_jupyter_ubuntu.sh
+++ setup_jupyter_ubuntu.sh
@@ -4,10 +4,11 @@ wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
 bash Anaconda3-2023.09-0-Linux-x86_64.sh
 #chmod u+x *.exp
 #./script.exp
+#
 source ~/.bashrc
+#
 conda install jupyter -yq
 #
 jupyter notebook --generate-config
 #
-cd Complete-Python-3-Bootcamp/
 nohup jupyter notebook --no-browser --port=8888 --ip=0.0.0.0 &
