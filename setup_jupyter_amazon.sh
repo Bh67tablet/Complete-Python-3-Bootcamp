fe33c925250fe3e550aea3de02e239b6aff7a689
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2024-01-02 10:17:28 +0100

    Update setup_jupyter_amazon.sh

--- setup_jupyter_amazon.sh
+++ setup_jupyter_amazon.sh
@@ -3,17 +3,17 @@ sudo yum -y install bzip2
 sudo yum -y install git
 sudo su - -c 'su - ec2-user -c "echo PATH=$PATH:/home/ec2-user/anaconda3/bin >> ~/.bash_profile"'
 #sudo su - -c 'su - ec2-user -c "source ~/.bashrc"'
-cat > /home/ec2-user/install_jp.sh << EOF
+git clone https://github.com/Bh67tablet/Complete-Python-3-Bootcamp.git
+cd Complete-Python-3-Bootcamp
+cat > install_jp.sh << EOF
 #/bin/bash!
 wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
 bash Anaconda3-2023.09-0-Linux-x86_64.sh -b
-git clone https://github.com/Bh67tablet/Complete-Python-3-Bootcamp.git
-cd Complete-Python-3-Bootcamp
 conda install jupyter -yq
 jupyter notebook --generate-config
 nohup jupyter notebook --no-browser --port=8888 --ip=0.0.0.0 &
 cat nohup.out
 EOF
-sudo chown ec2-user:ec2-user /home/ec2-user/install_jp.sh
-sudo chmod u+x /home/ec2-user/install_jp.sh
+sudo chown ec2-user:ec2-user install_jp.sh
+sudo chmod u+x install_jp.sh
 sudo su - -c 'su - ec2-user -c "source ~/.bashrc & ./install_jp.sh"'
