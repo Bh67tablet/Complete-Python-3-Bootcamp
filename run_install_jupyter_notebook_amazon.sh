4dbde726323de9843a4714e99fbaf5f42cdeec44
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2024-01-02 14:03:00 +0100

    Update run_install_jupyter_notebook_amazon.sh

--- run_install_jupyter_notebook_amazon.sh
+++ run_install_jupyter_notebook_amazon.sh
@@ -4,4 +4,4 @@ sudo yum -y install git
 sudo su - -c 'su - ec2-user -c "echo PATH=$PATH:/home/ec2-user/anaconda3/bin >> ~/.bash_profile"'
 sudo su - -c 'su - ec2-user -c "git clone https://github.com/Bh67tablet/Complete-Python-3-Bootcamp.git"'
 sudo chmod u+x /home/ec2-user/Complete-Python-3-Bootcamp/*.sh
-sudo su - -c 'su - ec2-user -c "source ~/.bashrc & /home/ec2-user/Complete-Python-3-Bootcamp/install_jp.sh"'
+sudo su - -c 'su - ec2-user -c "source ~/.bashrc & /home/ec2-user/Complete-Python-3-Bootcamp/install_jupyter_notebook_amazon.sh"'
