2cd40788ae989eab162137fecc44873340afcd2a
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2024-01-02 21:01:47 +0100

    Update install_jupyter_notebook_amazon.sh

--- install_jupyter_notebook_amazon.sh
+++ install_jupyter_notebook_amazon.sh
@@ -4,5 +4,5 @@ wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
 bash Anaconda3-2023.09-0-Linux-x86_64.sh -b
 conda install jupyter -yq
 jupyter notebook --generate-config
-nohup jupyter notebook --no-browser --port=8888 --ip=0.0.0.0 > /home/ec2-user/Complete-Python-3-Bootcamp/out.log 2>&1 &
+nohup jupyter notebook --no-browser --port=8888 --ip=0.0.0.0 >> /home/ec2-user/Complete-Python-3-Bootcamp/out.log 2>&1 &
 cat /home/ec2-user/Complete-Python-3-Bootcamp/out.log
