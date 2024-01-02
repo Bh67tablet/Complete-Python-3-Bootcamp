75a457aa65aeedf922a3c1c082e457b517e73b0a
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2024-01-02 11:24:17 +0100

    Update setup_jupyter_amazon.sh

--- setup_jupyter_amazon.sh
+++ setup_jupyter_amazon.sh
@@ -10,7 +10,7 @@ bash Anaconda3-2023.09-0-Linux-x86_64.sh -b
 conda install jupyter -yq
 jupyter notebook --generate-config
 nohup jupyter notebook --no-browser --port=8888 --ip=0.0.0.0 &
-#cat nohup.out
+cat nohup.out
 EOF
 sudo chown ec2-user:ec2-user /home/ec2-user/Complete-Python-3-Bootcamp/install_jp.sh
 sudo chmod u+x /home/ec2-user/Complete-Python-3-Bootcamp/install_jp.sh
