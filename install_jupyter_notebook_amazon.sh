2b424f19d4190bff6acd4d6ef210a18631602e7a
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2024-01-02 20:36:28 +0100

    Update install_jupyter_notebook_amazon.sh

--- install_jupyter_notebook_amazon.sh
+++ install_jupyter_notebook_amazon.sh
@@ -1,4 +1,4 @@
-#/bin/bash!
+#! /bin/bash
 cd /home/ec2-user/Complete-Python-3-Bootcamp
 wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
 bash Anaconda3-2023.09-0-Linux-x86_64.sh -b
